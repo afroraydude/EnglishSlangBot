971ccdb2cc6c167e5faa016061c5d542cd7f310e
root <root@afroraydude.com>
2017-01-29 00:30:11 -0500

    Verbosity

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 # TODO: VERBOSITY
+# ^ DONE! :)
+
 import praw # external stuff
 import time # python stuff
 import sys # delete this
@@ -11,7 +13,7 @@ subreddit = reddit.subreddit("BritBot") # testing subreddit
 
 commented = None # delete this
 
-def us_convert(parent):
+def us_convert(parentverbose=True):
         hasUS = False
         c = "ERROR RECIEVED"
         f = MyParser()
@@ -23,14 +25,16 @@ def us_convert(parent):
                         c = c.replace(key, words['us'][key])
                         hasUS = True
         except:
+            if verbose:
                 print "CANNOT GET PARENT BODY"
         if hasUS:
+            if verbose:
                 print "Translated"
         else:
-                c = "There is no british slang here!"
+                c = "There is no US slang here!"
         return c
 
-def brit_convert(parent):
+def brit_convert(parent,verbose=True):
 	hasBrit = False
         c = "ERROR RECIEVED"
 	f = MyParser()
@@ -39,51 +43,53 @@ def brit_convert(parent):
 	try:
 		c = parent # parent comment's body (what we translate)
 		for key in words['brit']: # all words in the British slang list
-			c = c.replace(key, words['brit'][key]) # Replaces the word with the translation 
+			c = c.replace(key, words['brit'][key]) # Replaces the word with the translation
        			hasBrit = True # See below for usage
 	except:
+            if verbose:
 		print "CANNOT GET PARENT BODY"
 	if hasBrit:
+            if verbose:
 		print "Translated"
 	else:
 		c = "There is no british slang here!"
 	return c
-def check_summon(c):		
+def check_summon(c):
     text = c.body
     if text.find("engslang!") != -1:
         return True
 def bot_action(c, verbose=True, respond=True):
     	if verbose:
-		test = "MessageCheck Started" 
+		test = "MessageCheck Started"
 	text = c.body
     	if verbose:
     	    print test
-        if respond and text.find("has been summoned") == -1:
+        if respond and text.find("has been summoned") == -1: # If it is me
             head = "SlangBot (English) has been summoned!\n\n" # Begining of message
 	    tail = "\n\n[^^^I ^^^am ^^^a ^^^bot](http://reddit.com/r/britbot/)\nYou can provide feedback in my subreddit: /r/BritBot :)" # end of message
-	 
+
             parent = c.parent()
 	    try:
 		check = parent.body
-		if text.find("brit") != -1:
-			fixed = "Translation: " + brit_convert(parent.body) 
-		else:
-			if text.find("us") != -1:
+		if text.find("brit") != -1: # If you use the Brit command
+			fixed = "Translation: " + brit_convert(parent.body)
+		else: # For other commands
+			if text.find("us") != -1: # US command
 				fixed = "Translation: " + us_convert(parent.body)
-		
-			else:
+
+			else: # Any command that is not registered
+			    if verbose:
 		    		print "Command does not exist"
-		try:
+		try: # Try to reply to the comment
                     c.reply(head + fixed + tail)
 		except praw.exception.APIException:
-		    print "Pausing for 2 mins"
+		    if verbose:
+			print "Pausing for 2 mins" # RATELIMIT time
 		    time.sleep(120)
 		    c.reply(head + fixed + tail)
                 with open("commented.txt", "a") as myfile:
-                    myfile.write("\n" + c.id)
-                
-		#    print "WAS KILL"
-		
+                    myfile.write("\n" + c.id) # So we know not to message again
+
             except AttributeError:
 		if verbose:
 			print "is submission or error, comment id " + c.id
